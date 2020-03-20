3ba186672e57cc9c9d015159fc4df2d6a5da1cf2
kolegm <kolegm.real@gmail.com>
2020-03-20 19:37:49 +0200

    feat: added app level. Include logic with IOC-container

---
+++ src/app/container.js
@@ -0,0 +1,36 @@
+import Bottle from 'bottlejs';
+
+import infrastructure from '../infrastructure';
+import domains from '../domains';
+
+import handlers from './handlers';
+
+import middlewares from './middlewares';
+import koa from './koa-instance';
+import routing from './routing';
+import application from './app';
+
+const bottle = new Bottle('StartApp');
+bottle.strict = true;
+
+infrastructure(bottle);
+domains(bottle);
+
+bottle.constant('middlewares', middlewares);
+bottle.constant('rootPath', '/input');
+
+handlers(bottle);
+
+bottle.serviceFactory(
+  'koaApp',
+  koa,
+  'middlewares',
+  'handler.error',
+  'handler.errorCatching'
+);
+
+bottle.factory('routing', routing);
+
+bottle.serviceFactory('application', application, 'koaApp', 'routing');
+
+export default bottle.container;
